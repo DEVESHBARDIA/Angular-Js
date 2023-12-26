5e3497024a37e46ccdc4edd42be1a0675d04cc62
DEVESH BARDIA <108572863+DEVESHBARDIA@users.noreply.github.com>
2023-12-26 09:25:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,79 @@ a.controller("fname", function($scope){
     $scope.firstname = "Devesh"
     $scope.lastname = "Bardia"
 })
+
+//SHOPPING CART
+
+var a = angular.module("shopping", []);
+a.controller("cart", function($scope){
+    $scope.items = ['mango','cherry','strawberry','apple']
+    $scope.add = function(){
+        if ($scope.additem && $scope.items.indexOf($scope.additem)==-1)
+        {
+           $scope.items.push($scope.additem);
+           $scope.additem="";
+        }
+        else{
+            if($scope.additem)
+            {
+                alert("Already exist");
+                document.getElementById("ip").focus();
+                return false;
+            }
+            else {
+                alert("please enter item to add");
+                document.getElementById("ip").focus();
+                return false;
+            }
+        }
+    }
+    $scope.remove=function()
+        {
+            if($scope.items.indexOf($scope.removeitem)==-1)
+            {
+                alert("Please select an item to remove");
+                document.getElementById("rm").focus()
+                return false; 
+            }
+            else{
+                var index=$scope.items.indexOf($scope.removeitem);
+                $scope.items.splice(index,1)
+                $scope.removeitem="";
+            }
+        } 
+})
+
+
+
+//CALCULATOR
+
+var a = angular.module("calculator", []);
+a.controller("cal", function($scope){
+    $scope.display='';
+    $scope.appendnumber = function(digit){
+        $scope.display += digit;
+    }
+
+    $scope.handleaction = function(action){
+        switch(action){
+            case 'C' :
+                $scope.display = "";
+                break;
+            case '=' :
+                try{
+                    $scope.display = eval($scope.display).toString();
+                    if($scope.display === 'Infinity' || $scope.display === '-Infinity'){
+                        throw 'Enter proper Denominator';
+                        $scope.display = "";
+                    }
+                } catch (error){
+                    $scope.display = '';
+                    alert("Invalid Input")
+                }
+                break;
+
+            default :
+            $scope.display += action;
+        }
+    }
+})
